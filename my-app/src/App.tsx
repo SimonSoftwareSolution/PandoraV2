4ee0a9eb53e63e628ba613613a070edda0dc4d39
Simon Poppinga <simon.poppinga@BlueHands.de>
2022-06-19 14:30:24 +0200

    Modal on welcome screen

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import titelbild from './images/titelbild.jpg';
+import React, { useState } from 'react';
+import wald from './images/wald2022.jpeg';
 import drache from './images/selbstgemalt.jpg';
 import { Link } from 'react-router-dom';
 import './App.css';
 import { url } from 'inspector';
+import { Button, Modal } from 'react-bootstrap';
 
 function App() {
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
+    <>
+    <Modal show={show} onHide={handleClose} centered>
+    <Modal.Body style={{backgroundColor: 'black' }}>
+      <img onClick={() => handleClose()} style={{borderRadius: 10, borderWidth: 50 , borderColor: 'black'}}  className="img-fluid" alt="" src={drache}/>
+    </Modal.Body>
+  
+  </Modal>
     <div className="App container d-flex flex-column h-100 mt-5">
       <div className="card" style={{backgroundColor: 'white'}}>
-        <img  className="img-fluid" alt="" src={drache}/>
+      <img  className="img-fluid" alt="" src={wald}/>
         <div className="card-body">
           <h5 className="card-title">Willkommen</h5>
           <p className="card-textfont-weight-bold">
@@ -19,7 +32,7 @@ function App() {
           Bitte kontrolliert eure Namen auf der Gästeliste, das ist eure „EINTRITTSKARTE“, da
           wir als private Veranstaltung angemeldet sind.<br/>
           Wenn Ihr Ideen, Fragen oder Anmerkungen habt, gerne her damit – wendet euch
-          einfach an euren Ansprechpartner des Vertrauens 🖤
+          einfach an euren Ansprechpartner:innen des Vertrauens 🖤
           </p>
         </div>
       </div>
@@ -36,6 +49,7 @@ function App() {
         <Link to="/lastyear" style={{color : 'black', textDecoration : 'none'}}><h2>Letztes Jahr</h2></Link>
       </div>
     </div>
+    </>
   );
 }
 
--- my-app/src/pages/anfahrt.tsx
+++ my-app/src/pages/anfahrt.tsx
@@ -8,11 +8,22 @@ export default function Anfahrt() {
           <div className="card-body mb-5">
             <h5 className="card-title">Lets go</h5>
             <p className="card-text">
-            Liebe Leute, wegen der Anfahrt: die Zielhaltestelle selbst heißt Brodersdorf und wird von der Buslinie 14 von Kiel aus bedient. Von dort aus lauft ihr noch ca. 15-20 Min. bis zum Gelände. Falls ihr schon gebucht habt, wären Laboe/ Röbsdorf, Probsteierhagen etc. auch nicht ganz falsch. 
-            Wenn der Aufbau gut läuft, (und das tut er, wenn ein paar helfende Hände schon früher anreisen und mit anpacken 😉)  findet sich bestimmt ein freundliche*r Fahrer*in, der/die Neuankömmlinge abholt. 
-            Wir versuchen schnellstmöglich eine Website auf die Beine zu stellen, wo der Fußweg beschrieben ist. 
-            Außerdem, besonders, wenn man gerne abgeholt werden möchte - bietet es sich an, in Fahrgemeinschaften zu kommen, auch dafür wird es über die Website nochmal ein Portal geben, wo ihr eure Reisezeit eintragen könnt (falls ihr wollt). Und Autofahrer ihre freien Plätze anbieten dürfen.
-            Auf ein gutes Gelingen und mit ganz viel Vorfreude auf unser Abenteuer "Pandora"! Liebe Grüße 🍀
+            Wer sein Rad dabei hat kommt damit tagsüber auch ganz gut an den Strand :-)<br/>
+            Aber beachtet die aktuell z.T. bedingte Mitnahmemöglichkeit im ÖPNV...<br/>
+            Bezüglich der Anfahrt: die Zielhaltestelle selbst heißt Brodersdorf und wird von der
+            Buslinie 14 von Kiel aus bedient. Von dort aus lauft ihr noch ca. 15-20 Min. bis zum
+            Gelände (dünne rote Linie). Falls ihr schon gebucht habt, wären Laboe/ Röbsdorf,
+            Probsteierhagen etc. auch nicht ganz falsch. Alternativ mit dem Schnellbus bis
+            Laboe, Brodersdorfer Weg, dafür solltet ihr aber abgesprochen haben, dass euch
+            jemensch abholt, sonst lauft ihr ein bisschen länger.<br/>
+            Außerdem gilt für stilvolles Anreisen: es gibt es den Fördedampfer von Kiel nach
+            Laboe.<br/>
+            Wenn mensch gerne abgeholt werden möchte - bietet es sich an, in
+            Fahrgemeinschaften zu kommen, auch dafür wird es über die Website nochmal ein
+            <a href='https://docs.google.com/spreadsheets/d/1Mhqwb4y6DHsK6bwsYZF0l2rPQ-wbOZMow2vKAd4jLdk/edit#gid=0'> Portal</a> geben, auch Autofahrer sind hier gebeten ihre Plätze nach Möglichkeit zur
+            Verfügung zu stellen. Auf ein gutes Gelingen und mit ganz viel Vorfreude auf unser
+            Abenteuer &quot;Pandora&quot;!
+            Liebe Grüße 🍀
             </p>
           </div>
       </div>
